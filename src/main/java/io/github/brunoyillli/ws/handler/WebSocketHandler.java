9121d6f75e509b83e1eba76d691464d743c8fecc
Bruno Mendes <wolfteam1910@gmail.com>
2023-04-25 11:19:43 -0300

    subindo projeto

---
+++ src/main/java/io/github/brunoyillli/ws/handler/WebSocketHandler.java
@@ -0,0 +1,9 @@
+package io.github.brunoyillli.ws.handler;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler{
+
+}
---
+++ src/test/java/io/github/brunoyillli/ws/WsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.brunoyillli.ws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
