7db05938965f6ac21937e9ed37563d79589f65ac
Bruno Mendes <wolfteam1910@gmail.com>
2023-04-25 15:27:11 -0300

    testando envio de mensagens

--- src/main/java/io/github/brunoyillli/ws/handler/WebSocketHandler.java
+++ src/main/java/io/github/brunoyillli/ws/handler/WebSocketHandler.java
@@ -1,7 +1,13 @@
 package io.github.brunoyillli.ws.handler;
 
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.UUID;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
@@ -12,6 +18,19 @@ public class WebSocketHandler extends TextWebSocketHandler{
 	@Override
 	public void afterConnectionEstablished(WebSocketSession session) throws Exception {
 		System.out.println("[afterConnectionEstablished] session id " + session.getId());
+		
+		new Timer().scheduleAtFixedRate(new TimerTask() {
+			
+			@Override
+			public void run() {
+				try {
+					session.sendMessage(new TextMessage("Olá " + UUID.randomUUID()));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}, 5000l, 5000L);
 	}
 
 	@Override
